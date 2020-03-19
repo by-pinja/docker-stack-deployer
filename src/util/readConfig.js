4c62a8cd5abd79a4f413955ea35c2c5d7d289ec1
tarvainen <atte.tarvainen@protacon.com>
2020-03-19 19:43:39 +0200

    Display informative error message if configuration file is invalid

--- src/util/readConfig.js
+++ src/util/readConfig.js
@@ -1,6 +1,19 @@
 const fs = require('fs')
 const path = require('path')
+const logger = require('./logger')
 
-const readConfig = () => JSON.parse(fs.readFileSync(path.join(__dirname, '../../', 'config.json')))
+const readConfig = () => {
+  const configFilePath = path.join(__dirname, '../../', 'config.json')
+
+  try {
+    return JSON.parse(fs.readFileSync(configFilePath))
+  } catch (e) {
+    logger.error(
+      `Failed to read configuration file at "${configFilePath}". Make sure it exists and it's valid JSON.`
+    )
+
+    throw e
+  }
+}
 
 module.exports = readConfig
